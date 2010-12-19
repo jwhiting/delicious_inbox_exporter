3dd97f56cad25095cd77bea861dc35a9135d382e
jwhiting <jwhiting@blockchalk.com>
2010-12-18 17:04:35 -0800

    initial commit

---
+++ inboxexporter.rb
@@ -0,0 +1,228 @@
+=begin
+
+Please see README for introductory information.
+
+************************************
+* OUTPUT CONFIGURATION
+************************************
+
+These defaults will work fine, but you may wish to change the way inbox items
+are tagged, how the sending user is referenced, etc.
+
+=end
+
+add_tags = 'inbox'              # these tags (space-separated) will be added to
+                                # every inbox item
+
+mark_private = true             # items will be marked private
+
+description_sender = true       # the description will be appended with
+                                # the sender in description_sender_format.
+
+description_sender_format = "[from %PROFILENAME%]"
+                                # %PROFILENAME% will be replaced with the
+                                # user's profile name. %USERNAME% will be
+                                # replaced with the user name. in many
+                                # cases these are the same value, if the
+                                # sending user has not setup a profile name in
+                                # their delicious account settings
+
+tag_sender = true               # add a tag for the sending user
+
+tag_sender_format = "from:delicious/%USERNAME%"
+                                # %USERNAME% will be replaced with the
+                                # delicious username. note that %PROFILENAME%
+                                # is not supported in this case.
+
+page_request_delay = 500        # how many milliseconds to wait between page
+                                # loads.
+
+=begin
+
+************************************
+* END OF CONFIGURATION
+************************************
+        
+=end
+
+require 'net/http'
+require 'uri'
+require 'rubygems'
+require 'hpricot'
+require 'cgi'
+require 'time'
+
+puts "
+This script does not automate the login process. You need to login to Delicious
+and then copy/paste your authenticated site cookies here. It's really quite
+easy:
+
+1. Sign in to Delicious
+
+2. Go to your inbox page to make sure you see your inbox items
+
+3. In the browser's address bar, enter this string verbatim and hit return:
+
+   javascript:alert(document.cookie)
+
+   This will pop up an alert with the contents of your cookies for the
+   delicious.com domain.
+
+4. Copy the entire blurb from the alert box and paste it here
+
+Note: this data will be discarded when the script completes for your security
+
+Please paste the blurb now, and hit return:
+
+"
+
+cookie = ''
+while (cookie.empty?) do
+  cookie << STDIN.gets
+  cookie.gsub!(/\A\s+|\s+\Z/,'')
+end
+puts ""
+
+# I've chosen to be a good citizen and present a custom user agent
+user_agent = "Delicious Inbox Exporter v0.1"
+
+# get delicious username from cookie
+if (cookie =~ /.*_user=([a-zA-Z90-9\_\.]+).*/)
+  username = $1
+  puts "Exporting inbox for delicious/#{username}"
+else
+  puts "Could not get your delicious username from the cookie data. "+
+       "Please check that you've followed the cookie instructions."
+  exit
+end
+
+# open output file
+out_fn = $ARGV[0]
+unless out_fn.to_s.length > 0
+  puts "Please specify an output file as the first argument"
+  exit
+end
+outfile = File.open(out_fn,"w") rescue nil
+unless outfile
+  outs "Could not open output file '#{out_fn}' for writing."
+  exit
+end
+
+# setup scraper loop
+items = []
+page = 1
+morepages = true
+
+while (morepages) do
+
+  puts "Fetching and parsing inbox page #{page}..."
+
+  # fetch the page and parse into hpricot doc
+  url = URI.parse("http://www.delicious.com/inbox/#{username}?page=#{page}&setcount=100")
+  path = url.path + (url.query ? "?"+url.query : "")
+  req = Net::HTTP::Get.new(path)
+  req["User-Agent"] = user_agent
+  req["Cookie"] = cookie
+  http = Net::HTTP.new(url.host, url.port)
+  res = http.start {|http|
+    http.request(req)
+  }
+  doc = Hpricot.parse(res.body)
+
+  # pull out the items
+  page_items = []
+  date_group = nil
+  doc.search("li").each { |li|
+    if li.classes.include? "post"
+      item = {
+        :tags => [],
+        :description => '',
+        :url => nil,
+        :title => nil,
+        :from_user_name => nil,
+        :from_user_profile_name => nil,
+        :date => nil,
+      }
+      li.search("div[@class~='dateGroup']").each do |d|
+        date_group = d.attributes['title']
+      end
+      item[:date] = date_group
+      li.search("a").each do |a|
+        if a.classes.include? "taggedlink"
+          item[:url] = a.attributes['href']
+          item[:title] = a.inner_text
+        end
+        if a.classes.include? "user"
+          item[:from_user_name] = a.attributes['href'].gsub(/^\//,'') 
+          item[:from_user_profile_name] = a.inner_text
+        end
+        if a.classes.include? "tag"
+          item[:tags] << a.inner_text.gsub(/\A\s+|\s+\Z/,'')
+        end
+      end
+      li.search("div[@class~='description']").each do |d|
+        item[:description] = d.inner_text.gsub(/\A\s+|\s+\Z/,'')
+      end
+      page_items << item
+    end
+  }
+  items.concat(page_items)
+  puts "Got #{page_items.count} items from page #{page} (#{items.count} total so far)"
+
+  # check pagination for more pages
+  morepages = false
+  doc.search("div[@id~='pagination']").each do |d|
+    d.search("a[@class~='next']").each do |a|
+      morepages = true
+    end
+  end
+  page += 1
+  sleep (page_request_delay/1000.0) if morepages
+
+  if items.empty?
+    puts "Oops, something has gone wrong - no inbox items were found."
+    puts "Please check that you've followed all the instructions."
+    puts "Possible causes:"
+    puts "- You didn't include the correct authenticated cookie values"
+    puts "- Your cookies have expired. Sign in again to Delicious and try again"
+    puts "- You don't have network access right now"
+    puts "- You have inbox data that this script wasn't designed to handle"
+    puts "- Delicious is down or has changed something"
+    puts "- Who knows?"
+    exit
+  end
+end
+
+# write output in netscape bookmark style
+outfile.puts '<!DOCTYPE NETSCAPE-Bookmark-file-1>'
+outfile.puts '<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">'
+outfile.puts "<TITLE>Bookmarks</TITLE>"
+outfile.puts "<H1>Bookmarks</H1>"
+outfile.puts "<DL><p>"
+
+items.each do |item|
+  add_tags.split(/\s+/).each do |tag|
+    item[:tags] << tag unless tag.empty?
+  end
+  if tag_sender
+    item[:tags] << tag_sender_format.gsub(/\%USERNAME\%/,item[:from_user_name])
+  end
+  if description_sender
+    desc = description_sender_format.dup
+    desc.gsub!(/\%USERNAME\%/,item[:from_user_name])
+    desc.gsub!(/\%PROFILENAME\%/,item[:from_user_profile_name])
+    item[:description] << (" " + desc)
+    item[:description].gsub!(/\A\s+/,'')
+  end
+  outfile.puts '<DT><A '+
+    'HREF="' + CGI::escapeHTML(item[:url]) + '" ' +
+    'ADD_DATE="' + Time.parse(item[:date]).to_i.to_s + '" '+
+    'PRIVATE="' + (mark_private ? "1" : "0") + '" '+
+    'TAGS="' + CGI::escapeHTML(item[:tags].join(",")) + '"' +
+    '>' + CGI::escapeHTML(item[:title]) + "</A>"
+  outfile.puts '<DD>' + CGI::escapeHTML(item[:description])
+end
+
+outfile.puts "</DL><p>"
+outfile.close
+puts "Exported #{items.count} total items to #{out_fn}"
